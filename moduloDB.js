617bbade6819410c673b07a67b975c5f310ca6c2
Brayan <bragonya@gmail.com>
2017-03-13 22:16:33 -0600

    servidor chat arquitectura

---
+++ moduloDB.js
@@ -0,0 +1,380 @@
+var semestreActual=2;
+var date = new Date();
+var anioActual = 2016;//date.getFullYear();
+var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'bases2',
+    database : 'nodeprueba'
+  });
+
+  connection.connect();
+
+exports.query=function(callback,nombre){
+  connection.query('SELECT * from usuario where nombre=?',[nombre], function(err, rows, fields) {
+    if (!err){
+      console.log('The solution is: ', JSON.stringify(rows));
+      callback(JSON.stringify(rows));
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      callback(err);
+    }
+  });
+}
+
+
+exports.autenticar=function(carne,pass,role,socket){
+  var tabla ="alumno"; var campo1 ="Carne"; var rol =1;
+  if(role=="Maestro"){
+    var tabla ="maestro";
+    var campo1="CodigoMaestro";
+    var rol =2;
+  }
+  var notes;
+  connection.query('SELECT * from '+tabla+' where '+campo1+'=? and Contrasena=?',[carne,pass], function(err, rows, fields) {
+    if (!err){
+      console.log('The solution is: ', JSON.stringify(rows));
+      if(rows.length>0){
+        if(rol!=2){
+          notes="{\"nombre\":\""+rows[0].Nombre+"\",\"carne\":\""+rows[0].Carne+"\",\"role\":\""+rol+"\"}";
+        }else{
+          notes="{\"nombre\":\""+rows[0].Nombre+"\",\"carne\":\""+rows[0].CodigoMaestro+"\",\"role\":\""+rol+"\"}";
+        }
+      }else{
+        notes="{\"error\":\"Sus datos son invalidos, por favor vuelva a intentarlo\"}";
+      }
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+                socket.emit("responseAutenticar", notes);
+            });
+}
+
+
+exports.getCursosMaestro=function(CodigoMaestro,socket){
+  var notes;
+  connection.query(`select Curso.Nombre,Maestro.Nombre As Catedratico,asignacionMaestro.fkSeccion as Seccion from asignacionMaestro 
+join Curso on fkCodigoCurso=CodigoCurso 
+join Maestro on CodigoMaestro=fkCodigoMaestro 
+where Maestro.CodigoMaestro=? and asignacionMaestro.fkSemestre=? and asignacionMaestro.fkAnio=?`,[CodigoMaestro,semestreActual,anioActual], function(err, rows, fields) {
+    if (!err){
+      //console.log('The solution is: ', JSON.stringify(rows));
+      console.log("EXTRACCION: "+rows[0].Nombre);
+      notes="{\"cursos\":[";
+      for(var i in rows){
+        notes+="{\"nombre\":\""+rows[i].Nombre+"\",\"catedratico\":\""+rows[i].Catedratico+"\",\"seccion\":\""+rows[i].Seccion+"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+
+              console.log("ENVIANDO A MAESTRO ---------------------"+CodigoMaestro);
+              console.log("salida on: "+notes);
+                socket.emit("recibirCursos", notes);
+            });
+}
+
+exports.getCursosAlumno=function(carne,socket){
+  console.log("Extrallendo cursos de "+carne);
+  var notes;
+  connection.query(`select Curso.Nombre,asignacionAlumno.fkSeccion as Seccion, (select COUNT(*) from Instancia
+where fkCodigoCursoAlumno=asignacionAlumno.fkCodigoCurso and fkCarne=asignacionAlumno.fkCarne and Instancia.fkSeccionAlumno=asignacionAlumno.fkSeccion and fkSemestreAlumno=asignacionAlumno.fkSemestre and fkAnioAlumno=asignacionAlumno.fkAnio and visto=0) as Contador
+from asignacionAlumno 
+join Curso on fkCodigoCurso=CodigoCurso
+where asignacionAlumno.fkCarne=? and fkSemestre=? and fkAnio=?
+group by Curso.Nombre,  Seccion`,[carne,semestreActual,anioActual], function(err, rows, fields) {
+    if (!err){
+      //console.log('The solution is: ', JSON.stringify(rows));
+      //console.log("EXTRACCION: "+rows[0].Nombre);
+      notes="{\"cursos\":[";
+      for(var i in rows){
+        notes+="{\"nombre\":\""+rows[i].Nombre+"\",\"catedratico\":\"\",\"seccion\":\""+rows[i].Seccion+"\",\"contador\":\""+rows[i].Contador+"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              //console.log("salida on: "+notes);
+              console.log("ENVIANDO A MAESTRO ---------------------"+carne);
+                socket.emit("recibirCursos", notes);
+            });
+}
+
+exports.insertMensajeMaestro=function(username,curso,seccion,mensaje,socket,app_users){
+  var notes;
+  connection.query(`insert into Mensaje(idMensaje,fkCodigoMaestro,fkCodigoCurso,fkSeccion,fkSemestre,fkAnio,mensaje) values(0,?,(select CodigoCurso
+from Curso where Nombre=?),?,?,?,?);`,[username,curso,seccion,semestreActual,anioActual,mensaje], function(err, rows, fields) {
+    if (!err){
+      notes=1;
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      notes=0;
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+              if(notes==1){
+                // console.log("estuiantes del curso seran extraidos");
+                // var cadena = "{\"curso\":\""+curso+"\",\"seccion\":\""+seccion+"\",\"mensaje\":\""+mensaje+"\"}";
+                // socket.emit("estudiantesRecipientes", cadena);
+                listaAlumnos(curso,seccion,mensaje,socket,app_users);
+              }
+            });
+}
+
+
+exports.asignarCurso=function(username,curso,seccion,socket){
+  var notes;
+  connection.query(`insert into AsignacionAlumno(fkCarne,fkCodigoCurso,fkSeccion,fkSemestre,fkAnio) values(?,(select CodigoCurso from Curso where nombre=?),?,?,?)`,[username,curso,seccion,semestreActual,anioActual], function(err, rows, fields) {
+    if (!err){
+      notes="exitoso";
+    }
+    else{
+      console.log('Error mientras se asignaba el usuario '+username+" al curso "+curso+":"+err);
+      notes=err;
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+            // console.log("estuiantes del curso seran extraidos");
+             var cadena = "{\"curso\":\""+curso+"\",\"seccion\":\""+seccion+"\",\"estado\":\""+notes+"\"}";
+             socket.emit("recibirAsignacionCurso", cadena);
+            });
+}
+
+function listaAlumnos(curso,seccion,mensaje,socket,app_users){
+  var notes;
+  connection.query(`select fkCarne as Carne, fkSeccion as Seccion, Curso.Nombre as Curso
+from asignacionAlumno
+join Curso on Curso.CodigoCurso=asignacionAlumno.fkCodigoCurso
+where asignacionAlumno.fkSemestre=? and asignacionAlumno.fkAnio=? and Curso.Nombre=? and asignacionAlumno.fkSeccion=?`,[semestreActual,anioActual,curso,seccion], function(err, rows, fields) {
+    if (!err){
+      notes=rows;
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      notes=0;
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+              if(notes!=0){
+                //socket.emit("enviarMensaje", notes,mensaje);
+                console.log("VOY A MANDAR MENSAJES EN TIEMPO REAL A: ");
+                for(i in notes){
+                  for(j in app_users){
+                    if(notes[i].Carne==app_users[j].username){
+                      console.log(i+" "+notes[i].Carne);
+                      socket.in(j).emit("recibirMensajes","{\"arreglo\":[{\"seccion\":\""+notes[i].Seccion+"\",\"visibilidad\":\""+0+"\",\"curso\":\""+notes[i].Curso+"\",\"mensaje\":\""+mensaje+"\",\"catedratico\":\"\"}]}");
+                      notificacionesAlumnos(notes[i].Carne,socket.in(j));
+                    }
+                  }
+
+                }
+              }
+            });
+}
+
+function notificacionesAlumnos(username,socket){
+  var notes;
+  connection.query(`select Curso.Nombre As Curso,Mensaje.mensaje As Mensaje,asignacionAlumno.fkSeccion as Seccion
+from Instancia  
+join asignacionAlumno on fkCodigoCursoAlumno=fkCodigoCurso and Instancia.fkCarne=asignacionAlumno.fkCarne and fkSeccionAlumno=fkSeccion and fkSemestreAlumno=fkSemestre and fkAnioAlumno=fkAnio
+join Curso on asignacionAlumno.fkCodigoCurso=CodigoCurso
+join Mensaje on Instancia.fkMensaje=Mensaje.idMensaje
+where asignacionAlumno.fkCarne=? and asignacionAlumno.fkSemestre=? and asignacionAlumno.fkAnio=? and Instancia.visto=0
+order by Mensaje.fecha desc limit 5;`,[username,semestreActual,anioActual], function(err, rows, fields) {
+    if (!err){
+      notes="{\"arreglo\":[";
+      for(var i in rows){
+        notes+="{\"seccion\":\""+rows[i].Seccion+"\",\"visibilidad\":\"0\",\"curso\":\""+rows[i].Curso+"\",\"mensaje\":\""+rows[i].Mensaje+"\",\"catedratico\":\"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error while performing Query.'+err);
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+                socket.emit("inbox", notes);
+            });
+}
+
+exports.cambiarVisibilidad=function(carne,curso,seccion){
+  console.log(carne+ "pide cambiar la visilidad de los mensajes del curso "+curso);
+  connection.query(`UPDATE Instancia 
+join asignacionAlumno on fkCodigoCursoAlumno=fkCodigoCurso and Instancia.fkCarne=asignacionAlumno.fkCarne and fkSeccionAlumno=fkSeccion and fkSemestreAlumno=fkSemestre and fkAnioAlumno=fkAnio
+join Curso on fkCodigoCursoAlumno=Curso.CodigoCurso
+SET visto=1 WHERE visto=0 and asignacionAlumno.fkCarne=? and Curso.Nombre=? and asignacionAlumno.fkSeccion=? and fkAnioAlumno=? and fkSemestreAlumno=?`,[carne,curso,seccion,anioActual,semestreActual], function(err, rows, fields) {
+    if (!err){
+      //console.log('The solution is: ', JSON.stringify(rows));
+      //console.log("EXTRACCION: "+rows[0].Nombre);
+      
+    }
+    else{
+      console.log('Error al cambiar la visibilidad de los mensajes de: .'+carne+', No es un potencial error');
+    }
+  });
+}
+
+exports.getTopAlumno=function(carne,curso,seccion,inf,sup,socket){
+  console.log(carne+ "pide mas mensajes del curso "+curso);
+  var notes;
+  connection.query(`select Curso.Nombre As Curso,Mensaje.mensaje As Mensaje,asignacionAlumno.fkSeccion as Seccion, Instancia.visto As Visibilidad
+from Instancia  
+join asignacionAlumno on fkCodigoCursoAlumno=fkCodigoCurso and Instancia.fkCarne=asignacionAlumno.fkCarne and fkSeccionAlumno=fkSeccion and fkSemestreAlumno=fkSemestre and fkAnioAlumno=fkAnio
+join Curso on asignacionAlumno.fkCodigoCurso=CodigoCurso
+join Mensaje on Instancia.fkMensaje=Mensaje.idMensaje
+where asignacionAlumno.fkCarne=? and asignacionAlumno.fkSemestre=? and asignacionAlumno.fkAnio=? and Curso.Nombre=? and asignacionAlumno.fkSeccion=?
+order by Mensaje.fecha desc limit `+inf+`,`+sup,[carne,semestreActual,anioActual,curso,seccion], function(err, rows, fields) {
+    if (!err){
+      notes="{\"arreglo\":[";
+      for(var i in rows){
+        notes+="{\"seccion\":\""+rows[i].Seccion+"\",\"visibilidad\":\""+rows[i].Visibilidad+"\",\"curso\":\""+rows[i].Curso+"\",\"mensaje\":\""+rows[i].Mensaje+"\",\"catedratico\":\"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error al recibirTop de: .'+carne+', No es un potencial error');
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+                socket.emit("recibirTop", notes);
+            });;
+}
+
+
+exports.getMensajesAlumno=function(carne,curso,seccion,socket){
+  console.log(carne+ "pide mas mensajes del curso "+curso);
+  var notes;
+  connection.query(`select Curso.Nombre As Curso,Mensaje.mensaje As Mensaje,asignacionAlumno.fkSeccion as Seccion, Instancia.visto As Visibilidad
+from Instancia  
+join asignacionAlumno on fkCodigoCursoAlumno=fkCodigoCurso and Instancia.fkCarne=asignacionAlumno.fkCarne and fkSeccionAlumno=fkSeccion and fkSemestreAlumno=fkSemestre and fkAnioAlumno=fkAnio
+join Curso on asignacionAlumno.fkCodigoCurso=CodigoCurso
+join Mensaje on Instancia.fkMensaje=Mensaje.idMensaje
+where asignacionAlumno.fkCarne=? and asignacionAlumno.fkSemestre=? and asignacionAlumno.fkAnio=? and Curso.Nombre=? and asignacionAlumno.fkSeccion=?
+order by Mensaje.fecha desc limit 10;`,[carne,semestreActual,anioActual,curso,seccion], function(err, rows, fields) {
+    if (!err){
+      notes="{\"arreglo\":[";
+      for(var i in rows){
+        notes+="{\"seccion\":\""+rows[i].Seccion+"\",\"visibilidad\":\""+rows[i].Visibilidad+"\",\"curso\":\""+rows[i].Curso+"\",\"mensaje\":\""+rows[i].Mensaje+"\",\"catedratico\":\"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error al recibirTop de: .'+carne+', No es un potencial error');
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+                socket.emit("recibirMensajes", notes);
+            });;
+}
+
+
+exports.getMensajesMaestro=function(codigo,curso,seccion,socket){
+  console.log(codigo+ "pide mas mensajes del curso "+curso);
+  var notes;
+  connection.query(`select Curso.Nombre As Curso,Mensaje.mensaje As Mensaje,Mensaje.fkSeccion as Seccion,Mensaje.fecha
+from Mensaje  
+join Curso on Mensaje.fkCodigoCurso=CodigoCurso
+where Mensaje.fkCodigoMaestro=? and Mensaje.fkSemestre=? and Mensaje.fkAnio=? and Curso.Nombre=? and Mensaje.fkSeccion=?
+order by Mensaje.fecha desc limit 10`,[codigo,semestreActual,anioActual,curso,seccion], function(err, rows, fields) {
+    if (!err){
+      notes="{\"arreglo\":[";
+      for(var i in rows){
+        notes+="{\"seccion\":\""+rows[i].Seccion+"\",\"visibilidad\":\"1\",\"curso\":\""+rows[i].Curso+"\",\"mensaje\":\""+rows[i].Mensaje+"\",\"catedratico\":\"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error al recibirTop de: .'+codigo+', No es un potencial error');
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+                socket.emit("recibirMensajes", notes);
+            });;
+}
+
+
+exports.getTopMaestro=function(codigo,curso,seccion,inf,sup,socket){
+  console.log(codigo+ "pide mas mensajes del curso "+curso);
+  var notes;
+  connection.query(`select Curso.Nombre As Curso,Mensaje.mensaje As Mensaje,Mensaje.fkSeccion as Seccion,Mensaje.fecha
+from Mensaje  
+join Curso on Mensaje.fkCodigoCurso=CodigoCurso
+where Mensaje.fkCodigoMaestro=? and Mensaje.fkSemestre=? and Mensaje.fkAnio=? and Curso.Nombre=? and Mensaje.fkSeccion=?
+order by Mensaje.fecha desc limit `+inf+`,`+sup,[codigo,semestreActual,anioActual,curso,seccion], function(err, rows, fields) {
+    if (!err){
+      notes="{\"arreglo\":[";
+      for(var i in rows){
+        notes+="{\"seccion\":\""+rows[i].Seccion+"\",\"visibilidad\":\"1\",\"curso\":\""+rows[i].Curso+"\",\"mensaje\":\""+rows[i].Mensaje+"\",\"catedratico\":\"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error al recibirTop de: .'+codigo+', No es un potencial error');
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+              console.log("salida on: "+notes);
+                socket.emit("recibirTop", notes);
+            });;
+}
+
+
+
+exports.getListadoCursos=function(socket){
+  //console.log(carne+ "pide mas mensajes del curso "+curso);
+  var notes;
+  connection.query(`select Curso.Nombre as Curso, asignacionMaestro.fkSeccion as Seccion, Maestro.Nombre as Maestro
+from asignacionMaestro
+join Curso on Curso.CodigoCurso=asignacionMaestro.fkCodigoCurso
+join Maestro on Maestro.CodigoMaestro=asignacionMaestro.fkCodigoMaestro
+where asignacionMaestro.fkSemestre=? and asignacionMaestro.fkAnio=?
+order by Curso.Nombre,asignacionMaestro.fkSeccion,Maestro asc`,[semestreActual,anioActual], function(err, rows, fields) {
+    if (!err){
+      notes="{\"arreglo\":[";
+      for(var i in rows){
+        notes+="{\"seccion\":\""+rows[i].Seccion+"\",\"curso\":\""+rows[i].Curso+"\",\"catedratico\":\""+rows[i].Maestro+"\"},";
+      }
+      notes = notes.slice(0, -1);
+      notes+="]}";
+    }
+    else{
+      console.log('Error al recibirTop de: .'+carne+', No es un potencial error');
+      notes="{\"error\":\""+err+"\"}";
+    }
+  }).on('end', function(){
+                console.log("salida listado: "+notes);
+                socket.emit("recibirListadoCursos", notes);
+            });;
+}
+
+exports.end =function(){
+  connection.end();
+  console.log("close DB");
+}
+  
---
+++ server.js
@@ -0,0 +1,170 @@
+var express = require('express');  
+var app = express();  
+var server = require('http').Server(app);  
+var io = require('socket.io')(server);
+var DB =require('./moduloDB.js');
+var app_users=[];
+var keypress = require('keypress');
+var contador=0;
+
+
+app.use(express.static('C:\\Users\\Brayan\\Documents\\node\\sala de chat'));
+
+app.get('/', function(req, res) {  
+  res.status(200).send("Hello World!");
+});
+
+
+
+// make `process.stdin` begin emitting "keypress" events
+keypress(process.stdin);
+
+//listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+  if (key.ctrl && key.name == 'x') {
+    process.exit();
+  }else if(key.ctrl && key.name == 'p'){
+    for(var socketid in app_users){
+    console.log("Usuario: "+app_users[socketid].username+" con socket: "+socketid);
+  }
+  console.log("pp");
+  }
+});
+
+// process.stdin.setRawMode(true);
+// process.stdin.resume();
+
+io.sockets.on('connection', function(socket) {  
+  console.log('Alguien se ha conectado con Sockets 1'+socket.id);
+  socket.on('app_user',function(cad){
+  	var user = JSON.parse(cad);
+  	app_users[this.id]=user;
+  	console.log("Usuario registrado: "+user.username);
+  //   for(var socketid in app_users){
+  //     if(app_users[socketid].username=="201213578"){
+  //   console.log("lista: "+app_users[socketid].username);
+  //   io.sockets.in(socketid).emit("inbox","{\"arreglo\":[{\"seccion\":\"A\",\"visibilidad\":\"1\",\"curso\":\"Mate1\",\"catedratico\":\"Zambrano\",\"mensaje\":\"Buenas noches jovenes\"},{\"seccion\":\"B\",\"curso\":\"Ortodoncia 1\",\"mensaje\":\"manana materiales\"},{\"seccion\":\"C\",\"curso\":\"Progra1\",\"mensaje\":\"Llevar computadora\"}]}");
+  //   console.log("acabo de enviar:   "+"{\"arreglo\":[{\"seccion\":\"A\",\"visibilidad\":\"1\",\"curso\":\"Mate1\",\"mensaje\":\"Buenas noches jovenes\"},{\"seccion\":\"B\",\"curso\":\"Ortodoncia 1\",\"mensaje\":\"manana materiales\"},{\"seccion\":\"C\",\"curso\":\"Progra1\",\"mensaje\":\"Llevar computadora\"}]}");
+  // }}
+  });
+
+  socket.on('listaCursosMaestro',function(cad){
+    var peticion = JSON.parse(cad);
+    if(app_users[this.id]){
+      console.log(app_users[this.id].username+" pidio su lista de cursos");
+      DB.getCursosMaestro(peticion.username,io.sockets.in(this.id));
+    }
+  
+  });
+
+  socket.on('enviarAsignacionCurso',function(cad){
+    var peticion = JSON.parse(cad);
+    if(app_users[this.id]){
+      console.log(app_users[this.id].username+" pidio asigrnarse el curso "+peticion.curso+" seccion: "+peticion.seccion);
+      DB.asignarCurso(peticion.username,peticion.curso,peticion.seccion,io.sockets.in(this.id));
+    }
+  
+  });
+
+  socket.on('listaCursosAlumno',function(cad){
+    if(app_users[this.id]){
+    var peticion = JSON.parse(cad);
+      console.log(app_users[this.id].username+" el alumno pidio su lista de cursos");
+      DB.getCursosAlumno(peticion.username,io.sockets.in(this.id));
+    }
+  
+  });
+
+
+  socket.on('getMensajesAlumno',function(cad){
+    if(app_users[this.id]){
+      var peticion = JSON.parse(cad);
+      console.log("JSON: "+cad);
+      console.log(app_users[this.id].username+" pidio su mensajes de curso "+peticion.curso);
+      DB.getMensajesAlumno(peticion.username,peticion.curso,peticion.seccion,io.sockets.in(this.id));
+    }
+  
+  });
+
+
+  socket.on('getMensajesMaestro',function(cad){
+    if(app_users[this.id]){
+      var peticion = JSON.parse(cad);
+      console.log("JSON: "+cad);
+      console.log(app_users[this.id].username+" pidio su mensajes de su curso curso "+peticion.curso);
+      DB.getMensajesMaestro(peticion.username,peticion.curso,peticion.seccion,io.sockets.in(this.id));
+    }
+  
+  });
+
+
+  socket.on('getTopMaestro',function(cad){
+    if(app_users[this.id]){
+      var peticion = JSON.parse(cad);
+      console.log("JSON: "+cad);
+      console.log(app_users[this.id].username+" pidio top de su curso curso "+peticion.curso);
+      DB.getTopMaestro(peticion.username,peticion.curso,peticion.seccion,peticion.inicio,peticion.final,io.sockets.in(this.id));
+    }
+  
+  });
+
+    socket.on('cambiarVisibilidad',function(cad){
+    var peticion = JSON.parse(cad);
+    DB.cambiarVisibilidad(peticion.username,peticion.curso,peticion.seccion);
+
+  
+  });
+
+    socket.on('getTopAlumno',function(cad){
+    var peticion = JSON.parse(cad);
+        if(app_users[this.id]){
+          console.log("inicio: "+peticion.inicio+" final: "+peticion.final);
+          DB.getTopAlumno(peticion.username,peticion.curso,peticion.seccion,peticion.inicio,peticion.final,io.sockets.in(this.id));
+        }
+  });
+
+
+  socket.on('autenticar',function(cad){
+    var peticion = JSON.parse(cad);
+    console.log("Quiere autenticarse: "+cad);
+    DB.autenticar(peticion.carne,peticion.pass,peticion.role,io.sockets.in(this.id));
+  });
+
+  socket.on('sendMessage',function(cad){
+    var peticion = JSON.parse(cad);
+    console.log("El maestro quiere enviar un mensaje: "+cad);
+    DB.insertMensajeMaestro(peticion.username,peticion.curso,peticion.seccion,peticion.mensaje,io.sockets,app_users);
+  
+  });
+
+  socket.on('getListadoCursos',function(){
+    if(app_users[this.id]){
+      DB.getListadoCursos(io.sockets.in(this.id));
+    }
+    
+  
+  });
+
+  socket.on('disconnect',function(user){
+    if(app_users[this.id]){
+    	console.log("se ha eliminado a "+app_users[this.id].username)
+    	delete app_users[this.id];
+      console.log("usuarios online: "+app_users.length);
+    }else{
+      console.log("Socket anonimo: "+this.id+" acaba de abandonar");
+    }
+  });
+
+  
+});
+
+
+
+
+
+
+
+server.listen(8081, function() {  
+  console.log("Servidor corriendo en http://localhost:8080");
+});
